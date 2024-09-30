8982dc618f760d6cf10659f98002a54ad8270d8c
Sei Yukinari <sei.yukinari@nextremer.com>
2024-09-30 09:51:45 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,45 @@
-import * as dotenv from 'dotenv';
-import * as line from '@line/bot-sdk';
-import { isHoliday } from './holidayUtils';
-import * as Types from '@line/bot-sdk/lib/types';
+import * as dotenv from 'dotenv'
+import * as line from '@line/bot-sdk'
+import { isHoliday } from './holidayUtils'
+import * as Types from '@line/bot-sdk/lib/types'
 
-dotenv.config();
+dotenv.config()
 
 // LINE API設定
-const config: line.ClientConfig = {
+const lineConfig: line.ClientConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN || '',
-};
-
-// チャネルID
-const groupID = process.env.LINE_GROUP_ID || '';
-
-// クライアントインスタンスを作成
-const client = new line.Client(config);
+}
 
 // メッセージを送信
 const broadcastMessage = async (date: Date) => {
   const today = date.toLocaleDateString('ja-JP', {
     day: 'numeric',
     weekday: 'short',
-  });
+  })
   try {
-    const goToSchoolMessages: Types.Message[] = [
-      { type: 'text', text: `${today}\n🏫登校のリアクションお願いします🙏` },
-      { type: 'text', text: '車:🚗' },
-      { type: 'text', text: '歩き:🏃' },
-      { type: 'text', text: 'その他(お休み等)' },
-    ];
-    const leavingSchoolMessages: Types.Message[] = [
-      { type: 'text', text: '🏠下校のリアクションお願いします🙏' },
-      { type: 'text', text: '車:🚗' },
-      { type: 'text', text: '歩き:🏃' },
-      { type: 'text', text: '歩き(途中):🏃⇒🏭⇒🚙' },
-      { type: 'text', text: 'その他(お休み等)' },
-    ];
-    // メッセージを送信
-    await client.pushMessage(groupID, goToSchoolMessages);
-    await client.pushMessage(groupID, leavingSchoolMessages);
+    const messages: Types.Message[] = [
+      {
+        type: 'text',
+        text: `${today}\n🏫登校のリアクションお願いします🙏\n車\n🚗⇒🏫`,
+      },
+      { type: 'text', text: '歩き\n🏃⇒🏫' },
+      { type: 'text', text: '🏠下校のリアクションお願いします🙏\n車\n🚗⇒🏠' },
+      { type: 'text', text: '歩き\n🏃⇒🏠' },
+      { type: 'text', text: '歩き(途中)\n🏃⇒🏭⇒🚙' },
+    ]
+    const groupID = process.env.LINE_GROUP_ID || ''
+    const client = new line.Client(lineConfig)
+    await client.pushMessage(groupID, messages)
   } catch (error: unknown) {
-    console.error('Error sending broadcast message:', error);
+    throw error
   }
-};
+}
 
-const today = new Date();
-today.setHours(today.getHours() + 9); // JSTに変換
+const today = new Date()
+today.setHours(today.getHours() + 9) // JSTに変換
 // 祝日ならメッセージを送信しない
 if (!isHoliday(today)) {
-  broadcastMessage(today).then(() => console.log('Done!'));
+  broadcastMessage(today)
+    .then(() => console.log('Done!'))
+    .catch((error) => console.error('Error:', error))
 }
